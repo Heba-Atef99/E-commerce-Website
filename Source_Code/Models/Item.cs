70c9d9af1b7d9385bc4a0286a0bfbbc652e6214a
Heba Atef <hoba30.ha@gmail.com>
2021-12-03 21:18:17 +0200

    first 4 models

---
+++ Source_Code/Models/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commerce.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Img { get; set; }
+        public DateTime Date { get; set; }
+        public int Original_count { get; set; }
+        public int Available_count { get; set; }
+        public int? Owner_account_id { get; set; }
+        [ForeignKey("Owner_account_id")]
+        public virtual Account Account { get; set; }
+
+    }
+}
---
+++ Source_Code/Models/Promoted_item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commerce.Models
+{
+    public class Promoted_item
+    {
+        [Key]
+        public int Id { get; set; }
+        public int? Item_id { get; set; }
+        [ForeignKey("Item_id")]
+        public virtual Item Item { get; set; }
+
+        public int? User_promoted_account_id { get; set; }
+        [ForeignKey("User_promoted_account_id")]
+        public virtual Account Account { get; set; }
+
+    }
+}
