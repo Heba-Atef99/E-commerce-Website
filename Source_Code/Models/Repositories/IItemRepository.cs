b916bf26ad47ea6603bd7335b48cd2068f3d8cff
Heba Atef <hoba30.ha@gmail.com>
2021-12-06 13:44:51 +0200

    add repositories

---
+++ Source_Code/Models/Repositories/IItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commerce.Models.Repositories
+{
+    public interface IItemRepository
+    {
+        IEnumerable<ITEM> GetAllItems(); 
+        IEnumerable<ITEM> GetItemsByAccId(int id); 
+        Boolean UpdateItem(ITEM itemChanges);
+        Boolean UpdateItemType(ITEM item, int newType);
+        Boolean UpdateItemType(ITEM oldItem, ITEM newItem, int newType);
+        Boolean DeleteItem(int typeId, int accId); 
+        Boolean RemoveAllItems(int accId);
+        void AddItem(ITEM i); 
+
+    }
+}
