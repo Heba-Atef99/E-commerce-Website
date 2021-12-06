f602b8b3146e4c963c195047379274eea56a47aa
nermeen_atef <34457317+nermeenatif@users.noreply.github.com>
2021-12-06 21:49:58 +0200

    add owner page

---
+++ OwnerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Routing;
+using E_commerce.Models.Repositories;
+using E_commerce.Models;
+
+namespace E_commerce.Controllers
+{
+    public class OwnerController : Controller
+    {
+        private readonly IItemRepository _itemRepo;
+
+        public OwnerController(IItemRepository itemRepository)
+        {
+            _itemRepo = itemRepository;
+        }
+        public IActionResult Owner()
+        {
+            IEnumerable<ITEM> t = _itemRepo.GetItemsByAccId(1);
+            return View(t);
+        }
+    }
+}
