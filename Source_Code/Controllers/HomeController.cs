ec17a42f800020762f9af89dc68ec181bb8c7d08
noura <1701595@eng.asu.edu.eg>
2021-12-07 20:49:25 +0200

    signup

---
+++ Source_Code/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using E_commerce.Models;
+using E_commerce.Models.Repositories;
+using E_commerce.View_Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAccountRepository _logger;
+        private readonly IUserRepository _user;
+        public HomeController(IAccountRepository logger, IUserRepository user)
+        {
+            _logger = logger;
+            _user = user;
+        }
+
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(Signup ob)
+        {
+            ACCOUNT acc = new ACCOUNT
+            {
+                Email = ob.Email,
+                Pass = ob.Password,
+            };
+            IEnumerable<ACCOUNT1> a = _logger.GetAllAccountEmailsAndPass();
+            foreach (var ac in a)
+            {
+                if(ac.Email == ob.Email) 
+                {
+                    ViewBag.EmailExistError = "You have already signed up";
+                    return Redirect("/Home/index");
+                }
+                
+            }
+
+                 _logger.AddAccount(acc);
+            USER u = new USER
+            {
+                Name = ob.Name,
+
+                Phone = ob.Phone,
+                Address = ob.Address,
+            };
+            _user.AddUser(u);
+
+            return Redirect("/Home/homepage");
+
+        }
+        public IActionResult Homepage()
+        {
+            IEnumerable<ACCOUNT1> a = _logger.GetAllAccountEmailsAndPass();
+            return View(a);
+        }
+    }
+}
---
+++ Source_Code/View Models/Signup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commerce.View_Models
+{
+    public class Signup
+    {
+        [Required]
+
+        public string Name { get; set; }
+        [Required]
+
+       // [StringLength(11)]
+        [DataType(DataType.PhoneNumber)]
+        public int Phone { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [MinLength(4)]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+}
