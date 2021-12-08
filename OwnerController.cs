67c40422cb1ccfd0732ef018289c225cb8d54e5d
nermeen_atef <34457317+nermeenatif@users.noreply.github.com>
2021-12-08 10:42:55 +0200

    edit in owner page

--- OwnerController.cs
+++ OwnerController.cs
@@ -13,15 +13,65 @@ namespace E_commerce.Controllers
     public class OwnerController : Controller
     {
         private readonly IItemRepository _itemRepo;
+        private readonly IPromotedItemRepository _PromotedItemRepo;
+        private readonly ICartRepository _CartRepo;
 
-        public OwnerController(IItemRepository itemRepository)
+        public OwnerController(IItemRepository itemRepository, IPromotedItemRepository PromotedItemRepoRepository, ICartRepository CartRepository)
         {
             _itemRepo = itemRepository;
+            _PromotedItemRepo = PromotedItemRepoRepository;
+            _CartRepo = CartRepository;
         }
-        public IActionResult Owner()
+         
+        public IActionResult Owner(int add, int share)
         {
-            IEnumerable<ITEM> t = _itemRepo.GetItemsByAccId(1);
+            int loginaccount = 2;
+            int owneraccount = 1;
+            
+            PROMOTED_ITEM promoted = new PROMOTED_ITEM();
+            CART cart = new CART();
+            if (share != 0)
+            {
+                
+                promoted.Item_Id = share;
+                promoted.Promoted_Account_Id = loginaccount;
+                _PromotedItemRepo.AddPromotedItem(promoted);
+            }
+            if (add != 0)
+            {
+                cart.Item_Id = add;
+                cart.Account_Id = loginaccount;
+                _CartRepo.AddToCart(cart);
+            }
+
+            IEnumerable<ITEM> t = _itemRepo.GetItemsByAccId(owneraccount);
+            IEnumerable<PROMOTED_ITEM> p= _PromotedItemRepo.GetPromotedItemsByAccId(owneraccount);
+            var result = p.Select(O => O.Item_Id)
+                        .ToList();
+            List<ITEM> PromotedItem = new List<ITEM>();
+            for (int i = 0; i < result.Count; i++)
+            {
+                ITEM l = _itemRepo.GetItemById(i);
+                PromotedItem.Add(l);
+            }
+
+
+            ViewBag.data1 = t;
+            ViewBag.data2 = PromotedItem;
+            
+            return View();
+        }
+        public IActionResult Deposit(string btn)
+        {
+            int t = 0;
+            if (btn != null)
+            {
+                 t = 10;
+            }
+            
             return View(t);
         }
+       
+
     }
 }
