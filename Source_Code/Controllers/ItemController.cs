4fbdb5c1e58250099ab525a0de6f89128b6cd1c6
Heba Atef <hoba30.ha@gmail.com>
2021-12-10 00:31:06 +0200

    upload dashboard and item page

--- Source_Code/Controllers/ItemController.cs
+++ Source_Code/Controllers/ItemController.cs
@@ -8,53 +8,122 @@ using Microsoft.AspNetCore.Routing;
 using E_commerce.Models.Repositories;
 using E_commerce.Models;
 using E_commerce.ViewModels.ItemVM;
+using E_commerce.Data;
 
 namespace E_commerce.Controllers
 {
     public class ItemController : Controller
     {
         private readonly IItemRepository _itemRepo;
+        private readonly ITypeRepository _typeRepo;
+        private readonly IPurchasedItemRepository _purchasedItemRepo;
 
-        public ItemController(IItemRepository itemRepository)
+        public ItemController(IItemRepository itemRepository, IPurchasedItemRepository purchasedItemRepo, ITypeRepository typeRepo)
         {
             _itemRepo = itemRepository;
+            _purchasedItemRepo = purchasedItemRepo;
+            _typeRepo = typeRepo;
         }
+
         public IActionResult Dashboard(string btn)
         {
             if(btn != null)
             {
                 HttpContext.Session.SetString("Item_Type", btn);
-                return Redirect("/Dashboard/Item");
+                return Redirect("/Item/Item");
             }
             return View();
         }
         public IActionResult Item()
         {
             string item_type = HttpContext.Session.GetString("Item_Type");
+            int Reg_Id = (int)HttpContext.Session.GetInt32("User_Reg_Id");
+            IEnumerable<TYPE> allTypes = _typeRepo.GetAllTypes();
+            IEnumerable<PURCHASED_ITEM> myPurchased = _purchasedItemRepo.GetPurchasedItemsByAccId(Reg_Id);
+            IEnumerable<displayed_item> itemList;
             if (item_type == "sold")
             {
+                IEnumerable<ITEM> myItems = _itemRepo.GetItemsByAccId(Reg_Id);
+                IEnumerable<PURCHASED_ITEM> allPurchasedItems = _purchasedItemRepo.GetAllPurchasedItems().
+                    GroupBy(item => item.Item_Id).
+                    Select(g => g.First()).
+                    ToList();
+
+                itemList = allPurchasedItems.Join(myItems, i1 => i1.Item_Id, i2 => i2.Id,
+                    (i1, i2) => new
+                    { 
+                        Name = i2.Name,
+                        Price = i2.Price,
+                        Type = i2.Type,
+                        Image = i2.Image,
+                        Publish_Date = i2.Date,
+                        Sold_Instances = i2.Original_Count - i2.Available_Count
+                    }).Join(allTypes, t1 => t1.Type, t2 => t2.Id,
+                    (t1, t2) => new displayed_item
+                    {
+                        Name = t1.Name,
+                        Price = t1.Price,
+                        Type = t2.Type,
+                        Image = t1.Image,
+                        Publish_Date = t1.Publish_Date,
+                        Sold_Instances = t1.Sold_Instances
+                    });
+
+                ViewBag.ItemType = 0;
             }
+
             else if (item_type == "unsold")
             {
+                //unsold items are the items with available count != 0
+                IEnumerable<ITEM> myItems = _itemRepo.GetAvailableItemsByAccId(Reg_Id).Where(t => t.Available_Count != 0).ToList();
+
+                itemList = myItems.Join(allTypes, i1 => i1.Type, i2 => i2.Id, 
+                    (i1, i2) => new displayed_item {
+                        Name = i1.Name,
+                        Price = i1.Price,
+                        Type = i2.Type,
+                        Image = i1.Image,
+                        Available_Count = i1.Available_Count,
+                        Publish_Date = i1.Date
+                    });
+                
+                ViewBag.ItemType = 1;
             }
+
             else if (item_type == "purchased")
             {
+                IEnumerable<ITEM> allItems = _itemRepo.GetAllItems();
+
+                itemList = myPurchased.Join(allItems, i1 => i1.Item_Id, i2 => i2.Id,
+                    (i1, i2) => new 
+                    {
+                        Name = i2.Name,
+                        Price = i2.Price,
+                        Type = i2.Type,
+                        Image = i2.Image,
+                        Purchased_Count = i1.Purchased_Count,
+                        Purchase_Date = i2.Date
+                    }).Join(allTypes, t1 => t1.Type, t2 => t2.Id,
+                    (t1, t2) => new displayed_item
+                    {
+                        Name = t1.Name,
+                        Price = t1.Price,
+                        Type = t2.Type,
+                        Image = t1.Image,
+                        Purchased_Count = t1.Purchased_Count,
+                        Purchase_Date = t1.Purchase_Date
+                    });
+
+                ViewBag.ItemType = 2;
             }
             else
             {
-                //return Redirect("/Dashboard/Dashboard");
+                return Redirect("/Item/Dashboard");
             }
-            ITEM tt = new ITEM {Id = 3, Name = "Batata", Type = 3, Owner_Account_Id = 1, Available_Count=5, 
-                Description = "Sweet Batata", Original_Count= 10, Price = 50, Date = DateTime.Now, Image="lnlnl"};
-            _itemRepo.AddItem(tt);
+            return View(itemList);
 
-            //_itemRepo.UpdateItemType(tt, 2);
-            //Boolean check = _itemRepo.DeleteItem(2, 2);
-            IEnumerable<ITEM> t = _itemRepo.GetAllItems();
-            //IEnumerable<ITEM> t = _itemRepo.GetItemsByAccId(2);
-
-            return View(t);
         }
+        
         public IActionResult Inventory()
         {
             int Reg_Id = (int)HttpContext.Session.GetInt32("User_Reg_Id");
---
+++ Source_Code/Data/displayed_item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace E_commerce.Data
+{
+    public class displayed_item
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Type { get; set; }
+        public string Image { get; set; }
+        public int Available_Count { get; set; }
+        public int Sold_Instances { get; set; }
+        public int Purchased_Count { get; set; }
+        public DateTime Publish_Date { get; set; }
+        public DateTime Purchase_Date { get; set; }
+    }
+}
