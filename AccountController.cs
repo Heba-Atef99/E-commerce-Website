0cd20207efa8236a7cbf2ca31a1b61b40652b6c2
nermeen_atef <34457317+nermeenatif@users.noreply.github.com>
2021-12-09 14:50:32 +0200

    edit in owner page

--- AccountController.cs
+++ AccountController.cs
@@ -10,27 +10,45 @@ using E_commerce.Models;
 
 namespace E_commerce.Controllers
 {
+    public class compositeitem
+    {
+        public ITEM item;
+        public string owner;
+        public string type;
+        public compositeitem(ITEM t, string name,string typename)
+        {
+            item = t;
+            owner = name;
+            type = typename;
+        }
+    }
     public class AccountController : Controller
     {
-        private readonly IItemRepository _itemRepo;
-        private readonly IPromotedItemRepository _PromotedItemRepo;
-        private readonly ICartRepository _CartRepo;
+        private readonly IItemRepository _itemRepository;
+        private readonly IPromotedItemRepository _PromotedItemRepository;
+        private readonly ICartRepository _CartRepository;
         private readonly IAccountRepository _AccountRepository;
+        private readonly IUserRepository _UserRepository;
+        private readonly ITypeRepository _TypeRepository;
         public AccountController(IItemRepository itemRepository,
             IPromotedItemRepository PromotedItemRepoRepository, ICartRepository CartRepository,
-            IAccountRepository AccountRepository)
+            IAccountRepository AccountRepository, IUserRepository UserRepository,
+            ITypeRepository TypeRepository)
         {
-            _itemRepo = itemRepository;
-            _PromotedItemRepo = PromotedItemRepoRepository;
-            _CartRepo = CartRepository;
+            _itemRepository = itemRepository;
+            _PromotedItemRepository = PromotedItemRepoRepository;
+            _CartRepository = CartRepository;
             _AccountRepository = AccountRepository;
-        }
+            _UserRepository = UserRepository;
+            _TypeRepository = TypeRepository;
+        } 
          
         public IActionResult Owner(int add, int share)
         {
-            int loginaccount = 1;
-            int owneraccount = 3;
-            
+            int loginaccount = 3;
+            //int loginaccount = (int)HttpContext.Session.GetInt32("User_Reg_Id");
+            int owneraccount = 1;
+
             PROMOTED_ITEM promoted = new PROMOTED_ITEM();
             CART cart = new CART();
             if (share != 0)
@@ -38,18 +56,31 @@ namespace E_commerce.Controllers
                 
                 promoted.Item_Id = share;
                 promoted.Promoted_Account_Id = loginaccount;
-                _PromotedItemRepo.AddPromotedItem(promoted);
+                _PromotedItemRepository.AddPromotedItem(promoted);
             }
             if (add != 0)
             {
                 cart.Item_Id = add;
                 cart.Account_Id = loginaccount;
                 cart.Item_count = 1;
-                _CartRepo.AddToCart(cart);
+                _CartRepository.AddToCart(cart);
             }
 
-            IEnumerable<ITEM> t = _itemRepo.GetItemsByAccId(owneraccount);
-            IEnumerable<PROMOTED_ITEM> p= _PromotedItemRepo.GetPromotedItemsByAccId(owneraccount);
+            IEnumerable<ITEM> t = _itemRepository.GetItemsByAccId(owneraccount);
+            //List<string> owner_name = new List<string>();
+            List<compositeitem> cat = new List<compositeitem>();
+            foreach (var k in t)
+            {
+                ITEM item = k;
+                int account_id = item.Owner_Account_Id;
+                int type_id = item.Type;
+                string type= _TypeRepository.GetTypeById(type_id).Type;
+                var user_id = _AccountRepository.GetAccountByAccId(account_id).User_Id;
+                string name = _UserRepository.GetUserById(user_id).Name;
+                compositeitem f = new compositeitem(item, name, type);
+                cat.Add(f);
+            }
+            IEnumerable<PROMOTED_ITEM> p = _PromotedItemRepository.GetPromotedItemsByAccId(owneraccount);
             var result = p.Select(O => O.Item_Id)
                         .ToList();
             List<ITEM> PromotedItem = new List<ITEM>();
@@ -57,20 +88,60 @@ namespace E_commerce.Controllers
             for (int i = 0; i < result.Count; i++)
             {
 
-                l = _itemRepo.GetItemById(result[i]);
-                 PromotedItem.Add(l);
+                l = _itemRepository.GetItemById(result[i]);
+                PromotedItem.Add(l);
+            }
+            List<compositeitem> cat2= new List<compositeitem>();
+            foreach (var k in PromotedItem)
+            {
+                ITEM item = k;
+                int account_id = item.Owner_Account_Id;
+                int type_id = item.Type;
+                string type = _TypeRepository.GetTypeById(type_id).Type;
+                var user_id = _AccountRepository.GetAccountByAccId(account_id).User_Id;
+                string name = _UserRepository.GetUserById(user_id).Name;
+                compositeitem f = new compositeitem(item, name, type);
+                cat2.Add(f);
             }
 
+            //List<TYPE> typeitem = new List<TYPE>();
+            //TYPE j = new TYPE();
+            //foreach(var item in t)
+            //{
+            //    j= _TypeRepository.GetTypeById(item.Id);
+            //    typeitem.Add(j);
+            //}
+            //var patient_join_follow_up = new List<Tuple<ITEM, TYPE>>();
+            //int k = 0;
+            //foreach ( ITEM  f in t)
+            //{
 
-            ViewBag.data1 = t;
-            ViewBag.data2 = PromotedItem;
+
+            //    patient_join_follow_up.Add(new Tuple<ITEM, TYPE>(f, typeitem[k]));
+            //    ++k;
+            //}
+
+            //List<string> owner_name = new List<string>();
+            //foreach (var item in t)
+            //{
+            //    int account_id = item.Owner_Account_Id;
+            //    var user_id = _AccountRepository.GetAccountByAccId(account_id).User_Id;
+            //    string name = _UserRepository.GetUserById(user_id).Name;
+            //    owner_name.Add(name);
+            //}
+            //ViewBag.owners = owner_name;
+
+
+            ViewBag.data1 = cat;
+            ViewBag.data2 = cat2;
             
             return View();
         }
         public IActionResult Deposit(string btn,string add)
         {
-            string t = "";
+            
             int loginaccount = 1;
+            //int loginaccount = (int)HttpContext.Session.GetInt32("User_Reg_Id");
             ACCOUNT account = _AccountRepository.GetAccountByAccId(loginaccount);
             //int balance = account.Balance;
             
