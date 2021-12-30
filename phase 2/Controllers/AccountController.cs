4cc7b19a0ba0634b288944b02036a9425a0a3bfb
nermeen_atef <34457317+nermeenatif@users.noreply.github.com>
2021-12-30 16:11:31 +0200

    edit in account controller

--- phase 2/Controllers/AccountController.cs
+++ phase 2/Controllers/AccountController.cs
@@ -16,25 +16,64 @@ namespace E_commerce.Controllers
     [Route("[controller]")]
     public class AccountController : ControllerBase
     {
-        private readonly IAccountRepository _logger;
-        private readonly IUserRepository _user;
-        private readonly IItemRepository _item;
-        public AccountController(IAccountRepository logger, IUserRepository user, IItemRepository item)
+        //private readonly IAccountRepository _logger;
+        //private readonly IUserRepository _user;
+        //private readonly IItemRepository _item;
+        //public AccountController(IAccountRepository logger, IUserRepository user, IItemRepository item)
+        //{
+        //    _logger = logger;
+        //    _user = user;
+        //    _item = item;
+        //}
+        private readonly IItemRepository _itemRepository;
+        private readonly IPromotedItemRepository _PromotedItemRepository;
+        private readonly ICartRepository _CartRepository;
+        private readonly IAccountRepository _AccountRepository;
+        private readonly IUserRepository _UserRepository;
+        private readonly ITypeRepository _TypeRepository;
+        public AccountController(IItemRepository itemRepository,
+            IPromotedItemRepository PromotedItemRepoRepository, ICartRepository CartRepository,
+            IAccountRepository AccountRepository, IUserRepository UserRepository,
+            ITypeRepository TypeRepository)
         {
-            _logger = logger;
-            _user = user;
-            _item = item;
+            _itemRepository = itemRepository;
+            _PromotedItemRepository = PromotedItemRepoRepository;
+            _CartRepository = CartRepository;
+            _AccountRepository = AccountRepository;
+            _UserRepository = UserRepository;
+            _TypeRepository = TypeRepository;
         }
         [HttpGet("{id}")]
         public ActionResult<IEnumerable<ITEM>> Owner(int id)
         {
-            return _item.GetItemsByAccId(id).ToArray();
+            return _itemRepository.GetItemsByAccId(id).ToArray();
+
+        }
+        [HttpGet("{id}/{item_id}")]
+        public ActionResult<IEnumerable<PROMOTED_ITEM>> Share_Item(int id,int item_id)
+        {
+            PROMOTED_ITEM promoted = new PROMOTED_ITEM();
+            promoted.Item_Id = item_id;
+            promoted.Promoted_Account_Id = id;
+            _PromotedItemRepository.AddPromotedItem(promoted);
+            return _PromotedItemRepository.GetPromotedItemsByAccId(id).ToArray();
+
+        }
+        [HttpGet("{id}/{item_id}")]
+        public ActionResult<IEnumerable<CART>> Add_To_Cart(int id, int item_id)
+        {
+            CART cart = new CART();
+            cart.Item_Id = item_id;
+            cart.Account_Id = id;
+            cart.Item_count = 1;
+            _CartRepository.AddToCart(cart);
+            return _CartRepository.GetCartByAccId(id).ToArray();
 
         }
         [HttpGet("{id}")]
         public ActionResult<int> Deposit(int id)
         {
-            return _logger.GetAccountByAccId(id).Balance;
+            return _AccountRepository.GetAccountByAccId(id).Balance;
 
         }
         [HttpPost]
@@ -42,10 +81,10 @@ namespace E_commerce.Controllers
         {
             int id = ob.Id;
             //addbalance = ob;
-            ACCOUNT account = _logger.GetAccountByAccId(id);
+            ACCOUNT account = _AccountRepository.GetAccountByAccId(id);
             //account.Balance = account.Balance + ob.Balance;
             account.Balance = account.Balance + ob.Balance;
-            bool k = _logger.UpdateAccount(account, 0);
+            bool k = _AccountRepository.UpdateAccount(account, 0);
             //return CreatedAtAction("Deposit", new ACCOUNT { Id = ob.Id }, ob);
             //return RedirectToAction("Deposit");
             //return RedirectToActionResult("Deposit",id);
